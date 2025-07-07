f8888b08273702549a0a6b61235aa0bf6ce4b220
EllisJLC <jia.liang.chen@ryerson.ca>
2025-07-07 03:25:26 -0400

    Created convertToInt

--- converters/convertToInt.js
+++ converters/convertToInt.js
@@ -1,12 +1,16 @@
 function convertToInt (array, options) {
+
+  let total;
     
     if (!options.reversed) {
         array.reverse()
     }
 
     for (let i = 0; i < array.length; i++) {
-        
+        total += (2 ** i) * array[i];
     }
+
+  return total;
 
 }
 
