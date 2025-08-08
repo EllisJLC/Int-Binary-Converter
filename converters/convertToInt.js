b7a181110e257d3bf5a138315565ca1916377097
EllisJLC <jia.liang.chen@ryerson.ca>
2025-08-08 01:27:01 -0400

    Fixed Int Converter

--- converters/convertToInt.js
+++ converters/convertToInt.js
@@ -4,16 +4,22 @@ function convertToInt (array, options) {
     options = {};
   }
 
+  array.map((item) => {
+    if (typeof item == "boolean") {
+      item = true ? 1 : 0;
+    } else if (item > 1) {
+      item = true;
+    }
+  })
+
   let total = 0;
     
     if (!options.reversed) {
         array.reverse()
     }
 
-    for (let i = 1; i < array.length; i++) {
-      if (array[i] == 1 || true) { 
-        total += (2 ** i);
-      }
+    for (let i = 0; i < array.length; i++) {
+        total += (2 ** i) * array[i];
     }
 
   return total;
