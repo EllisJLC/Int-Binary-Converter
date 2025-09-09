fc84970aa416245eb25d1a9ce8be9a183317a640
EllisJLC <97536806+EllisJLC@users.noreply.github.com>
2025-09-09 15:13:01 -0400

    Fixed variable naming in convertToBin.js

--- converters/convertToBin.js
+++ converters/convertToBin.js
@@ -6,20 +6,20 @@ function convertToBin (integer, size, options) {
         options = {}; // Fill to avoid errors?
     }
 
-    let pass = options.algebra ? 1 : true; // Set values to numbers if algebra option is set
-    let fail = options.algebra? 0 : false;
+    let one = options.algebra ? 1 : true; // Set values to numbers if algebra option is set
+    let zero = options.algebra? 0 : false;
 
     let array = new Array(size); // Initialize array
 
-    array.fill(fail); // Fill with false/0's
+    array.fill(zero); // Fill with false/0's
 
     while (integer >= 1) { // Continue to iterate downward
         if (integer >= 2 ** size) {
-            array[size] = pass;
+            array[size] = one;
             integer -= 2 ** size;
             size--;
         } else {
-            array[size] = fail;
+            array[size] = zero;
             size--;
         }
     }
